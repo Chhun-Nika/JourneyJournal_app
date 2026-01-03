1fad4b7fb3e9ee74dd633ab3408fcfa807e1f488
Sotheara Kon <sothearakon73@gmail.com>
2026-01-03 13:49:11 +0700

    create textfield and button widget

--- lib/ui/auth/login_screen.dart
+++ lib/ui/auth/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:journey_journal_app/ui/auth/register_screen.dart';
+import 'package:journey_journal_app/ui/shared/app_button.dart';
+import 'package:journey_journal_app/ui/shared/app_text_field.dart';
+import 'package:journey_journal_app/ui/utils/validators.dart';
 
 
 class LoginScreen extends StatefulWidget {
@@ -11,16 +14,28 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final _nameController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   void dispose() {
     super.dispose ();
-    _nameController.dispose();
+    _passwordController.dispose();
     _emailController.dispose();
   
   } 
+
+  // void login 
+  void onLogin () {
+    if (_formKey.currentState!.validate()) {
+      final email = _emailController.text;
+      final password = _passwordController.text;
+
+      // bug
+      debugPrint('Login with $email / $password');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,94 +43,58 @@ class _LoginScreenState extends State<LoginScreen> {
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(18),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Welcome back to Journey Journal!", style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),),
-            const SizedBox(height: 40,),
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(
-                label: Text(
-                  "name", 
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                filled: true,
-                fillColor: const Color(0xFFF7F7F7),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none
-                ),
-              ),       
-            ),
-            const SizedBox(height: 10,),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                label: Text(
-                  "email", 
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                filled: true,
-                fillColor: const Color(0xFFF7F7F7),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none
-                ),
-              ),       
-            ),
-            const SizedBox(height: 30,),
-            ElevatedButton(
-      
-              onPressed: () => {}, 
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xff89A4F1),
-                elevation: 4,
-                shadowColor: Colors.black,
-                minimumSize: const Size(double.infinity, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Welcome back to Journey Journal!", style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),),
+              const SizedBox(height: 40,),
+              AppTextField(
+                hint: "email", 
+                controller: _emailController, 
+                validator: Validators.email, 
+                keyboardType: TextInputType.emailAddress,
               ),
-              child: const Text(
-                "Log in",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
+              const SizedBox(height: 10,),
+              AppTextField(
+                hint: "password", 
+                controller: _passwordController, 
+                validator: Validators.password, 
+                obscureText: true,
               ),
-            ) ,
-      
-            const SizedBox(height: 12,),
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                text: "Didn’t have an account yet? ",
-                children: [
-                  TextSpan(
-                    text: "Sign up",
-                    style: const TextStyle(
-                      color: Colors.blue,
+              const SizedBox(height: 30,),
+              AppButton(
+                text: "login", 
+                onPressed: onLogin
+              ),
+              const SizedBox(height: 12,),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  text: "Didn’t have an account yet? ",
+                  children: [
+                    TextSpan(
+                      text: "Sign up",
+                      style: const TextStyle(
+                        color: Colors.blue,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterScreen(),
+                          ),
+                        );
+                      },
                     ),
-                    recognizer: TapGestureRecognizer()
-                    ..onTap = () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const RegisterScreen(),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         ),  
       ),
     );
--- lib/ui/auth/register_screen.dart
+++ lib/ui/auth/register_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:journey_journal_app/ui/auth/login_screen.dart';
+import 'package:journey_journal_app/ui/shared/app_button.dart';
+import 'package:journey_journal_app/ui/shared/app_text_field.dart';
+import 'package:journey_journal_app/ui/utils/validators.dart';
 
 class RegisterScreen extends StatefulWidget {
-  const RegisterScreen ({super.key});
+  const RegisterScreen({super.key});
 
   @override
   State<RegisterScreen> createState() => _SignupScreenState();
 }
 
 class _SignupScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
@@ -24,139 +28,99 @@ class _SignupScreenState extends State<RegisterScreen> {
     _confirmController.dispose();
   }
 
+  // sign up function
+  void onSignUp() {
+    if (_formKey.currentState!.validate()) {
+      final name = _nameController.text;
+      final email = _emailController.text;
+      final password = _passwordController.text; 
+
+      // bug 
+      debugPrint('$name register with $email / $password');
+    }
+  }
+
+  String? _confirmPasswordValidator(String? value) {
+    if (value != _passwordController.text) {
+      return 'Passwords do not match';
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(18),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Welcome to Journey Journal!", style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),),
-            const SizedBox(height: 40,),
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(
-                label: Text(
-                  "name", 
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                filled: true,
-                fillColor: const Color(0xFFF7F7F7),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none
-                ),
-              ),       
-            ),
-            const SizedBox(height: 10,),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                label: Text(
-                  "email", 
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                filled: true,
-                fillColor: const Color(0xFFF7F7F7),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none
-                ),
-              ),       
-            ),
-
-            const SizedBox(height: 10,),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                label: Text(
-                  "password", 
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                filled: true,
-                fillColor: const Color(0xFFF7F7F7),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none
-                ),
-              ),       
-            ),
-
-            const SizedBox(height: 10,),
-            TextField(
-              controller: _confirmController,
-              decoration: InputDecoration(
-                label: Text(
-                  "confirm password", 
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                filled: true,
-                fillColor: const Color(0xFFF7F7F7),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none
-                ),
-              ),       
-            ),
-            const SizedBox(height: 30,),
-            ElevatedButton(
-      
-              onPressed: () => {}, 
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xff89A4F1),
-                elevation: 4,
-                shadowColor: Colors.black,
-                minimumSize: const Size(double.infinity, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Welcome to Journey Journal!",
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
               ),
-              child: const Text(
-                "Sign up",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
+              const SizedBox(height: 40),
+              AppTextField(
+                hint: "name",
+                controller: _nameController,
+                validator: (v) => Validators.required(v, 'Name'),
               ),
-            ) ,
-      
-            const SizedBox(height: 12,),
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                text: "Already have an account?",
-                children: [
-                  TextSpan(
-                    text: "Log in",
-                    style: const TextStyle(
-                      color: Colors.blue,
+              const SizedBox(height: 10),
+              AppTextField(
+                hint: "email",
+                controller: _emailController,
+                validator: Validators.email,
+                obscureText: true,
+              ),
+              const SizedBox(height: 10),
+              AppTextField(
+                hint: "password",
+                controller: _passwordController,
+                validator: Validators.password,
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+              AppTextField(
+                hint: "comfirm password",
+                controller: _confirmController,
+                validator: _confirmPasswordValidator,
+                obscureText: true,
+              ),
+              const SizedBox(height: 30),
+              AppButton(
+                text: "Sign up", 
+                onPressed: onSignUp
+              ),
+              const SizedBox(height: 12),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  text: "Already have an account?",
+                  children: [
+                    TextSpan(
+                      text: "Log in",
+                      style: const TextStyle(color: Colors.blue),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const LoginScreen(),
+                            ),
+                          );
+                        },
                     ),
-                    recognizer: TapGestureRecognizer()
-                    ..onTap = () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const LoginScreen(),
-                        ),
-                      );
-                    },
-                  ),
-                ],
+                  ],
+                ),
               ),
-            )
-          ],
-        ),  
+            ],
+          ),
+        ),
       ),
     );
   }
